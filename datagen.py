e96822c630402e0cc51a883237a65b5b1d3da33c
euleo <eugenio.leonetti90@gmail.com>
2018-08-23 10:02:01 +0200

    First commit

---
+++ datagen.py
@@ -0,0 +1,155 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+import random
+import math
+import scipy.io
+
+from tensorflow.python.keras.applications.vgg16 import preprocess_input
+
+class DataGenerator(tf.keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, counting_dataset, labels, ranking_dataset, batch_size=25, dim=(224,224), n_channels=3, shuffle=True, rank_images=5):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.counting_dataset = counting_dataset
+        self.ranking_dataset = ranking_dataset
+        self.n_channels = n_channels
+        self.shuffle = shuffle
+        self.rank_images = rank_images
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.ranking_dataset) / self.rank_images)) # rank_images è il numero di immagini di ranking che prendo per ogni batch (da ognuna genero 5 sottopatch)
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.rank_images:(index+1)*self.rank_images] # indici delle 5 immagini di ranking da cui creo il batch di 25 immagini
+        
+        # counting_indexes = random.sample(range(0, len(self.counting_dataset)), self.batch_size)
+        counting_indexes = [random.randint(0, len(self.counting_dataset)-1) for _ in range(self.batch_size)]
+          
+        counting_dataset_temp = [self.counting_dataset[k] for k in counting_indexes]
+        list_ranking_imgs_temp = [self.ranking_dataset[k] for k in indexes] 
+
+        # Generate data
+        X, y = self.__data_generation(counting_dataset_temp,list_ranking_imgs_temp)
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(self.rank_images)
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, counting_dataset_temp,list_ranking_imgs_temp):
+        'Generates data containing batch_size samples'
+        # Initialization
+        X_counting = np.empty((self.batch_size, *self.dim, self.n_channels)) #counting batch
+        X_ranking = np.empty((self.batch_size, *self.dim, self.n_channels)) #ranking batch
+        y = np.empty((self.batch_size,14,14,1)) #counting batch target
+        y_tmp = np.empty((self.batch_size,14,14)) #usato per salvare temporaneamente il resize del counting target
+        
+        #Generate counting batch
+        for i, image in enumerate(counting_dataset_temp):
+            img = cv2.imread(image)
+            height, width = img.shape[:-1]            
+            
+            ####From PAPER: During training we sample one sub-image from each training image per epoch
+            #From PAPER: To further improve the performance of our baseline network, we introduce multi-scale sampling from the available
+            #labeled datasets. Instead of using the whole image as an input, we randomly sample square patches of varying size (from 56 to 448 pixels).
+            random_size = random.randint(min(56,width,height), min(448,width,height))#select random size
+            
+            random_x1 = random.randint(0, width-random_size)#select random point (the left-top corner of the patch)
+            random_y1 = random.randint(0, height-random_size)
+            crop_img = img[random_y1:random_y1+random_size, random_x1:random_x1+random_size]            
+            
+            res = cv2.resize(crop_img,(224,224),interpolation = cv2.INTER_LINEAR)#INTER_LINEAR - a bilinear interpolation
+            
+            img_array = np.asarray(res)
+            
+            X_counting[i,] = img_array
+            
+            dmap = self.labels[image]
+            crop_dmap = dmap[random_y1:random_y1+random_size, random_x1:random_x1+random_size]
+            
+            #resize to CNN output
+            y_tmp[i] = scipy.misc.imresize(crop_dmap, (14,14), interp='bilinear')
+            y[i] = np.resize(y_tmp[i],(14,14,1))
+
+        #### ALGORITHM TO GENERATE RANKED DATASETS ####
+        k = 5 #number of patches
+        s = 0.75 #scale factor
+        r = 8  #anchor region            
+        for i, image in enumerate(list_ranking_imgs_temp):            
+            img = cv2.imread(image)
+            height, width = img.shape[:-1]                    
+            
+            #select anchor region: crop a patch 1/r of original image centered in the same point and with same aspect ratio
+            anchor_region_width = width * math.sqrt(1/r)
+            anchor_region_height = height * math.sqrt(1/r)
+            
+            center_x = width/2
+            center_y = height/2
+            
+            x1 = int(round(center_x - (anchor_region_width/2)))
+            y1 = int(round(center_y - (anchor_region_height/2)))  
+            x2 = int(round(center_x + (anchor_region_width/2)))
+            y2 = int(round(center_y + (anchor_region_height/2)))
+            
+            #anchor region
+            # crop_img = img[y1:y2, x1:x2]
+                
+            #STEP 1: choose an anchor point ramdomly from the anchor region
+            anchor_point_x = random.uniform(x1, x2)
+            anchor_point_y = random.uniform(y1, y2)
+            
+            #STEP 2: find the largest square patch centered at the anchor point and contained within the image boundaries
+            anchor_point_offset_x = width - anchor_point_x
+            anchor_point_offset_y = height - anchor_point_y
+            
+            coord_list = [anchor_point_x, anchor_point_y, anchor_point_offset_x, anchor_point_offset_y]
+            patch1_half_width = min(coord_list)
+                
+            patch1_x1 = int(round(anchor_point_x - patch1_half_width))
+            patch1_y1 = int(round(anchor_point_y - patch1_half_width))
+            patch1_x2 = int(round(anchor_point_x + patch1_half_width))
+            patch1_y2 = int(round(anchor_point_y + patch1_half_width))
+            
+            #first_patch
+            crop_img1 = img[patch1_y1:patch1_y2, patch1_x1:patch1_x2]
+            crop_list = list()
+            crop_list.append(crop_img1)
+            res0 = cv2.resize(crop_img1,(224,224),interpolation = cv2.INTER_LINEAR)
+            res_array = np.asarray(res0)
+
+            X_ranking[i*k,] = res_array
+            
+            #STEP 3: Crop k-1 additional square patches, reducing size iteratively by a scale factor s. Keep all patches centered at anchor point    
+            for j in range(2, k+1):
+                img = crop_list[j-2]
+                h, w = img.shape[:-1]
+                
+                crop_width = w * math.sqrt(s) #same as height
+                
+                cen_x = w/2
+                cen_y = h/2
+                
+                xy1 = int(round(cen_x - (crop_width/2)))
+                xy2 = int(round(cen_x + (crop_width/2)))
+
+                crop_img2 = img[xy1:xy2, xy1:xy2]
+                crop_list.append(crop_img2)
+                
+                res_tmp = cv2.resize(crop_img2,(224,224),interpolation = cv2.INTER_LINEAR)
+                img_array = np.asarray(res_tmp)
+
+                X_ranking[(i*k)+j-1,] = img_array   
+        y_ranking = np.zeros((self.batch_size,1,1,1)) # dummy ranking batch target
+        X_counting = preprocess_input(X_counting)
+        X_ranking = preprocess_input(X_ranking)
+        return {'counting_input': X_counting, 'ranking_input': X_ranking}, {'conv2d_1': y, 'average_pooling2d_1': y_ranking}
---
+++ keras_script.py
@@ -0,0 +1,162 @@
+import numpy as np
+import glob
+import os
+import tensorflow as tf
+import cv2
+import scipy.io
+import scipy.signal
+import math     
+import random
+import itertools
+
+from tensorflow.python.keras.callbacks import TensorBoard
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.models import Model, Sequential
+from tensorflow.python.keras.layers import Input, Conv2D, AveragePooling2D
+from tensorflow.python.keras.applications import VGG16
+from datagen import DataGenerator
+from tensorflow.python.keras.optimizers import SGD
+from scipy.ndimage.filters import gaussian_filter 
+from scipy.linalg import block_diag
+
+tbCallBack = TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+
+def load_gt_from_mat(gt_file, gt_shape):
+    gt = np.zeros(gt_shape, dtype='float32')
+    mat_contents = scipy.io.loadmat(gt_file)
+    oct_struct = mat_contents['annPoints']
+    for dot in oct_struct:
+        gt[int(math.floor(dot[1]))-1,int(math.floor(dot[0]))-1] = 1.0
+    sigma = 15
+    gt = gt[:, :, 0]
+    density_map = gaussian_filter(gt, sigma)
+    return density_map
+    
+def pairwiseRankingHingeLoss(yTrue,yPred):    
+    kvar = K.constant(M)
+
+    yPred = K.squeeze(yPred, 3)
+    yPred = K.squeeze(yPred, 2)
+    
+    differences = K.dot(kvar,yPred)
+    
+    var = K.zeros(shape=(50, 1))
+    
+    max_tensor = K.maximum(differences,var)
+    
+    ranking_loss = K.sum(max_tensor)
+    return ranking_loss    
+    
+batch_size = 25 #25 for counting and 25 for ranking
+
+#matrice usata nella pairwiseRankingHingeLoss per fare i confronti tra le 5 sottopatch di una immagine di ranking
+Block_matrix = np.zeros((10,5))
+for i in range(0,4):
+    for j in range(0,5):
+        if j == 0:
+            Block_matrix[i][j] = -1
+        if j == i+1:
+            Block_matrix[i][j] = 1 
+
+Block_matrix[4:7,1:5] = Block_matrix[0:3,0:4]          
+Block_matrix[7:9,2:5] = Block_matrix[0:2,0:3]        
+Block_matrix[9:10,3:5] = Block_matrix[0:1,0:2] 
+
+#numero di Block_matrix che mi servono (per ogni 5 immagini di ranking mi serve una Block_matrix)
+blocks_number = int(round(batch_size/5)) 
+blocks_list = list()
+for i in range(0,blocks_number):
+    blocks_list.append(Block_matrix)
+M = block_diag(*blocks_list) #matrice usata nella pairwiseRankingHingeLoss
+
+# Counting Dataset
+counting_dataset = list()
+
+counting_dataset_path = 'counting_data_UCF_training'
+for im in glob.glob(os.path.join(counting_dataset_path, '*.jpg')):
+    counting_dataset.append(im)
+
+# Counting ground truth    
+labels = {}
+for i in range(len(counting_dataset)):
+    img = cv2.imread(counting_dataset[i])
+    gt_file = counting_dataset[i].replace('.jpg','_ann.mat')
+    dmap = load_gt_from_mat(gt_file, img.shape)
+    labels[counting_dataset[i]] = dmap
+
+#Ranking Dataset    
+ranking_dataset = list()
+
+ranking_dataset_path = 'ranking_data_training'
+for im in glob.glob(os.path.join(ranking_dataset_path, '*.jpg')):
+    ranking_dataset.append(im)
+
+# # Design model
+model = VGG16(include_top=True, weights='imagenet') #include_top=False to get only the convolutional part of VGG16
+transfer_layer = model.get_layer('block5_conv3')
+conv_model = Model(inputs=[model.input], outputs=[transfer_layer.output])
+
+counting_input = Input(shape=(224, 224, 3), dtype='float32', name='counting_input')
+ranking_input = Input(shape=(224, 224, 3), dtype='float32', name='ranking_input')
+x = conv_model([counting_input,ranking_input])
+#a single convolutional layer (a single 3x3x512 filter with stride 1 and zero padding to maintain same size)
+counting_output = Conv2D(1, (3, 3), strides=(1, 1), padding='same', data_format=None, dilation_rate=(1, 1), activation='relu', use_bias=True, kernel_initializer='glorot_uniform', bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(x)
+ranking_output = AveragePooling2D(pool_size=(14, 14), strides=None, padding='valid', data_format=None)(counting_output)
+new_model = Model(inputs=[counting_input,ranking_input], outputs=[counting_output,ranking_output])
+new_model.summary()
+
+optimizer = SGD(lr=0.000001, decay=0.0005, momentum=0.0, nesterov=False)
+loss={'conv2d_1': 'mean_squared_error', 'average_pooling2d_1': pairwiseRankingHingeLoss}
+loss_weights=[1,100]
+metrics = {'average_pooling2d_1': ['mae', 'mse']}
+
+new_model.compile(optimizer=optimizer,
+              loss=loss,
+			  metrics=metrics,
+              loss_weights=loss_weights)
+
+print('##################FIT GENERATOR######################################################################')
+# Parameters
+params = {'dim': (224,224),
+          'batch_size': batch_size, 
+          'n_channels': 3,
+          'shuffle': False,
+          'rank_images': int(round(batch_size/5))} # numero di immagini di ranking da prendere per creare un batch (da ogni immagine vengono prese 5 sottopatch)
+          
+split_train_labels = {}
+split_val_labels = {}
+
+split_size = int(round(len(counting_dataset)/5))
+splits_list = list()
+for t in range(0,5):
+    splits_list.append(counting_dataset[t*split_size:t*split_size+split_size])    
+
+# 5-fold cross validation
+for f in range(0,5):
+    print('Folder '+str(f))
+
+    splits_list_tmp = splits_list.copy()
+    
+    #counting validation split
+    split_val = splits_list_tmp[f]
+    
+    del splits_list_tmp[f]    
+    flat=itertools.chain.from_iterable(splits_list_tmp)
+    
+    #counting train split
+    split_train = list(flat)    
+    
+    #counting train split labels
+    split_train_labels = {k: labels[k] for k in split_train}
+    
+    #counting validation split labels
+    split_val_labels = {k: labels[k] for k in split_val}
+        
+    #ranking train split
+    split_rank_train = ranking_dataset[0:50]
+        
+    #train
+    # new_model.fit_generator(generator=DataGenerator(split_train, split_train_labels, split_rank_train, **params), epochs=1, callbacks=[tbCallBack])
+    
+    #train+validation
+    new_model.fit_generator(generator=DataGenerator(split_train, split_train_labels, split_rank_train, **params), validation_data=DataGenerator(split_val, split_val_labels, split_rank_train, **params), epochs=1, callbacks=[tbCallBack])
